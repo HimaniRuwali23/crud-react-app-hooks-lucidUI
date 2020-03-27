2791620cd739e0d7871c4cd78f247a25b3d2401c
himani <hlmani.ruwali@coachusa.com>
2020-03-27 23:27:07 +0530

    add basic react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React,  {useState}  from 'react';
-import empList from './data.js';
-import EmployeesData from './View/EmployeesData';
 import { Button, Panel } from 'lucid-ui';
 
 const App = () => {
-  const [empData, setEmpData] = useState(empList);
+  
   return (
     <div className="container">
       <div className="row">
@@ -14,7 +12,7 @@ const App = () => {
     <Panel.Header>
       <strong>View Employees</strong>
     </Panel.Header>
-        <EmployeesData empData={empData}/>
+        
         <Panel.Footer>
         <Button type="button" kind="primary" size="large">Add Employee</Button>
         </Panel.Footer>
