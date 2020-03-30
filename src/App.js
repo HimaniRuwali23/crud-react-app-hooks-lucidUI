c9bdffb48c76a6c6cf846b9a1e70c8e9dc82010a
himani <hlmani.ruwali@coachusa.com>
2020-03-30 22:32:54 +0530

    code commit for PR correction and adding sorting for list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React,  {useState}  from 'react';
-import empList from './data.js';
-import EmployeesData from './View/EmployeesData';
-import { Button, Panel } from 'lucid-ui';
+import React, { useState } from "react";
+import empList from "./data.js";
+import EmployeesData from "./View/EmployeesData";
+import { Button, Panel } from "lucid-ui";
 
 const App = () => {
   const [empData, setEmpData] = useState(empList);
@@ -10,18 +10,19 @@ const App = () => {
       <div className="row">
         <h1> List of Employees</h1>
       </div>
-    <Panel>
-    <Panel.Header>
-      <strong>View Employees</strong>
-    </Panel.Header>
-    <EmployeesData empData={empData}/>
+      <Panel>
+        <Panel.Header>
+          <strong>View Employees</strong>
+        </Panel.Header>
+        <EmployeesData empData={empData} />
         <Panel.Footer>
-        <Button type="button" kind="primary" size="large">Add Employee</Button>
+          <Button type="button" kind="primary" size="large">
+            Add Employee
+          </Button>
         </Panel.Footer>
-       
-       </Panel>
-       </div>
+      </Panel>
+    </div>
   );
-}
+};
 
 export default App;
--- src/View/EmployeesData.jsx
+++ src/View/EmployeesData.jsx
@@ -1,90 +1,88 @@
-import React, {useState} from 'react';
-import { Button, DataTable } from 'lucid-ui';
-import _ from 'lodash';
+import React, { useState } from "react";
+import { DataTable } from "lucid-ui";
+import _ from "lodash";
 
+const EmployeesData = props => {
+  const [activeIndex, setActiveIndex] = useState(1);
+  const [currentlySortedField, setCurrentlySortedField] = useState("id");
+  const [
+    currentlySortedFieldDirection,
+    setCurrentlySortedFieldDirection
+  ] = useState("down");
+  const [data, setData] = useState(props.empData.dtRowData);
+  const [dtColumn, setDtColumn] = useState(props.empData.dtColumnData);
 
-const EmployeesData = (props) => {
-    const initialEmpState = {
-        activeIndex: 1,
-        currentlySortedField: 'id',
-        currentlySortedFieldDirection: 'down',
-        data:props.empData
-      }
+  const handleSelect = (item, selectedIndex) => {
+    setData(
+      _.map(data, (row, rowIndex) => {
+        if (rowIndex === selectedIndex) {
+          return { ...row, isSelected: !row.isSelected };
+        } else {
+          return row;
+        }
+      })
+    );
+  };
 
-      
+  const handleRowClick = (item, rowIndex) => {
+    setActiveIndex(rowIndex);
+  };
 
-    const [emp] = useState(initialEmpState)
-    return (
-        <div>
-            <style>
-                {
-                '.child { display: none; } .lucid-Table-Tr:hover .child { display: block; }'
-                }
-            </style>
-            <DataTable
-          data={_.map(emp.data, (row, index) =>
-            index === emp.activeIndex ? { ...row, isActive: true } : row
-          )}
-          density='extended'
-          isFullWidth
-          minRows={4}
-          //isSelectable
-          isActionable
-        >
-        <DataTable.Column
-            field='id'
-            width={41}
-            align='left'
-            hasBorderLeft
-            isSortable
-            isSorted={emp.currentlySortedField === 'id'}
-            sortDirection={emp.currentlySortedFieldDirection}
-          >
-            ID
-          </DataTable.Column>
+  const handleSort = field => {
+    const nextCurrentlySortedFieldDirection =
+      currentlySortedField === field && currentlySortedFieldDirection === "up"
+        ? "down"
+        : "up";
+    const nextData = _.sortBy(data, field);
+    setCurrentlySortedField(field);
+    setCurrentlySortedFieldDirection(nextCurrentlySortedFieldDirection);
+    setData(
+      nextCurrentlySortedFieldDirection === "down"
+        ? nextData
+        : _.reverse(nextData)
+    );
+    setActiveIndex(null);
+  };
+
+  const dtData = () => {
+    setData(
+      _.map(data, (row, index) =>
+        index === activeIndex ? { ...row, isActive: true } : row
+      )
+    );
+  };
+
+  return (
+    <div>
+      <DataTable
+        data={data}
+        density="extended"
+        onSelect={handleSelect}
+        isFullWidth
+        minRows={4}
+        isSelectable
+        isActionable
+        onRowClick={handleRowClick}
+        onSort={handleSort}
+      >
+        {dtColumn.map((column, index) => (
           <DataTable.Column
-            field='name'
-            width={100}
-            align='left'
+            field={column.field}
+            width={column.width}
+            align={column.align}
             hasBorderLeft
             isSortable
-            isSorted={emp.currentlySortedField === 'id'}
-            sortDirection={emp.currentlySortedFieldDirection}
+            isSorted={currentlySortedField === column.field}
+            sortDirection={currentlySortedFieldDirection}
           >
-            Name
+            {column.title}
           </DataTable.Column>
-          <DataTable.Column
-            field='designation'
-            width={100}
-            align='left'
-            hasBorderLeft
-            isSortable
-            isSorted={emp.currentlySortedField === 'id'}
-            sortDirection={emp.currentlySortedFieldDirection}
-          >
-            Designation
-          </DataTable.Column>
-       
-          <DataTable.Column
-            field='project'
-            width={100}
-            align='left'
-            hasBorderLeft
-            isSortable
-            isSorted={emp.currentlySortedField === 'id'}
-            sortDirection={emp.currentlySortedFieldDirection}
-          >
-            Project
-          </DataTable.Column>
-       
-          <DataTable.Column field='edit' align='center' width={50}/>
-          <DataTable.Column field='delete' align='center' width={50}/>
-        
-       
-        
-          </DataTable>
-        </div>
-    )
-}
+        ))}
+        <DataTable.Column field="edit" align="center" width={50} />
+        <DataTable.Column field="delete" align="center" width={50} />
+      </DataTable>
+    </div>
+  );
+};
 
 export default EmployeesData;
